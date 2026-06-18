9fe1209c20912497da86ca9309ccb3bf7d732128
agrawalshubham7351-droid <agrawalshubham7351@gmail.com>
2026-06-18 22:22:23 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,9 +1,1 @@
-from delta_rest_client import DeltaRestClient
-
-delta_client = DeltaRestClient(
-    base_url='https://cdn-ind.testnet.deltaex.org',
-    api_key='JVZeR2ohMtPJlgaYej3djrYafpD5Vx',
-    api_secret='GbyBHI6BYaf5Osa5AMwSx6Drb583b5uCCafNtQah1ibMgvzXs3cwkCXyXhzG'
-)
-
-print(delta_client.get_position(84))
+# deleted
