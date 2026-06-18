fbdb6e38d26b4963994b01c80e7e14d91cbb12f7
agrawalshubham7351-droid <agrawalshubham7351@gmail.com>
2026-06-18 22:56:11 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -69,7 +69,11 @@ def webhook():
 
     return "OK", 200
 
-app.run(host="0.0.0.0", port=5001)
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5001))
+    )
 
 
 
